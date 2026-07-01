434810ae5a50523d5fc70d8f240f21fe36296a3d
Niksata <niki.kolev2006@gmail.com>
2026-07-01 12:36:34 +0300

    can't show message

--- pizza_app/views.py
+++ pizza_app/views.py
@@ -2,8 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-# def home(request):
-#     return render(request, 'pizza_app/index.html')
-
 def index(request):
-    return HttpResponse("Welcome to the Pizza App!")
+    return render(request, 'pizza_app/index.html')
