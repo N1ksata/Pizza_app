f47cd6b308eb3f171b392e59e1391a23aef91ba0
Niksata <niki.kolev2006@gmail.com>
2026-07-09 07:13:54 +0300

    final step for adding the whole menu

--- pizza_app/views.py
+++ pizza_app/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'pizza_app/index.html')
+
+def menu(request):
+    return render(request, 'pizza_app/menu.html')
--- pizza_project/urls.py
+++ pizza_project/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pizza_app.urls')),
+    path('menu/', include('pizza_app.urls')),
 ]
